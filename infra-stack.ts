9f250e659a0fcf8c173ec5c3db0a509c22ae9d6e
Hawwa11 <hawwazohair@gmail.com>
2021-02-27 16:03:48 +0800

    adds config values and vpc subnets

--- infra-stack.ts
+++ infra-stack.ts
@@ -9,6 +9,65 @@ export class InfraStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    //-----------VPC, SUBNETS-------------
+    
+    const publicSubnets = [
+      {
+        subnetType: ec2.SubnetType.PUBLIC,
+        name: 'public1',
+        cidrMask: config.get("SUBNET_CIDR_MASK")
+      },
+      {
+        subnetType: ec2.SubnetType.PUBLIC,
+        name: 'public2',
+        cidrMask: config.get("SUBNET_CIDR_MASK")
+      },
+      {
+        subnetType: ec2.SubnetType.PUBLIC,
+        name: 'public3',
+        cidrMask: config.get("SUBNET_CIDR_MASK")
+      }
+    ]
+    
+    //cannot reach internet at all
+    const isolatedSubnets = [
+      {
+        subnetType: ec2.SubnetType.ISOLATED,
+        name: 'isolated1',
+        cidrMask: config.get("SUBNET_CIDR_MASK"),
+      },
+      {
+        subnetType: ec2.SubnetType.ISOLATED,
+        name: 'isolated2',
+        cidrMask: config.get("SUBNET_CIDR_MASK"),
+      },
+      {
+        subnetType: ec2.SubnetType.ISOLATED,
+        name: 'isolated3',
+        cidrMask: config.get("SUBNET_CIDR_MASK"),
+      }
+    ]
+    //private subnets can reach internet via NAT Gateway
+    const privateSubnets = [
+      {
+        subnetType: ec2.SubnetType.PRIVATE,
+        name: 'private1',
+        cidrMask: config.get("SUBNET_CIDR_MASK"),
+      },
+      {
+        subnetType: ec2.SubnetType.PRIVATE,
+        name: 'private2',
+        cidrMask: config.get("SUBNET_CIDR_MASK"),
+      },
+      {
+        subnetType: ec2.SubnetType.PRIVATE,
+        name: 'private3',
+        cidrMask: config.get("SUBNET_CIDR_MASK"),
+      }
+    ]
+
+ 
+
     //-----------ECR-------------
     
     const ecr_repo_platform = new ecr.Repository(this, this.get_logical_env_name('ecr'), {
@@ -28,20 +87,22 @@ export class InfraStack extends cdk.Stack {
 
       cidr: config.get("VPC_IP"),
       natGateways: 0,
-      // subnetConfiguration: [...publicSubnets, ...isolatedSubnets,] //...privateSubnets,
+     // subnetConfiguration: [...publicSubnets, ...isolatedSubnets,...privateSubnets] //...privateSubnets,
     });
 
+ 
     //-----------RDS-------------
 
     const db_instance = new rds.DatabaseInstance(this, 'db-instance', {
-      vpc,
+      vpc : vpc,
       engine: rds.DatabaseInstanceEngine.postgres({
         version: rds.PostgresEngineVersion.VER_10
       }),      
       publiclyAccessible: false,
       credentials: rds.Credentials.fromGeneratedSecret(config.get('DB_USERNAME')),
       databaseName: `${config.get('DB_NAME')}`,
-      instanceIdentifier: `${config.get('DB_NAME')}`
+      instanceIdentifier: `${config.get('DB_NAME')}`,
+    
     });
 
     new cdk.CfnOutput(this, 'DB endpoint', { value: db_instance.instanceEndpoint.hostname });
