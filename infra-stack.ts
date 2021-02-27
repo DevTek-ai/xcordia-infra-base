087a14dd59cbff7061fe9f45e77973b2b8a8c9bb
Hussain Mansoor <hussain.mansoor@rea-group.com>
2021-02-27 14:08:33 +0800

    Adds RDS and VPC in infra base

--- infra-stack.ts
+++ infra-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as ecr from '@aws-cdk/aws-ecr';
+import * as rds from '@aws-cdk/aws-rds';
+import * as ec2 from '@aws-cdk/aws-ec2';
 
 const config = require('config');
 
@@ -19,6 +21,35 @@ export class InfraStack extends cdk.Stack {
         }
       ],
     });
+
+    
+    //-----------VPC-------------
+    const vpc = new ec2.Vpc(this, this.get_logical_env_name('vpc'), {
+
+      cidr: config.get("VPC_IP"),
+      natGateways: 0,
+      // subnetConfiguration: [...publicSubnets, ...isolatedSubnets,] //...privateSubnets,
+    });
+
+    //-----------RDS-------------
+
+    const db_instance = new rds.DatabaseInstance(this, 'db-instance', {
+      vpc,
+      engine: rds.DatabaseInstanceEngine.postgres({
+        version: rds.PostgresEngineVersion.VER_10
+      }),      
+      publiclyAccessible: false,
+      credentials: rds.Credentials.fromGeneratedSecret(config.get('DB_USERNAME')),
+      databaseName: `${config.get('DB_NAME')}`,
+      instanceIdentifier: `${config.get('DB_NAME')}`
+    });
+
+    new cdk.CfnOutput(this, 'DB endpoint', { value: db_instance.instanceEndpoint.hostname });
+    new cdk.CfnOutput(this, 'DB name', { value: db_instance.instanceIdentifier });
+    new cdk.CfnOutput(this, 'Secret Name', { value: db_instance.secret?.secretName! });
+
+
+
   }
 
   get_logical_env_name(resource_type: string): string {
